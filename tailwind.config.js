a212c8634f600745a0232ba824b62514427a698f
BranBayou <berhan.baye@gmail.com>
2023-07-30 23:36:27 +0300

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./login-page/public/**/*.html', './login-page/src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
