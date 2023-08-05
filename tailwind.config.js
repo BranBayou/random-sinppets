03d2f7a2b7639672eb96cf100842aa4e246fef4c
BranBayou <berhan.baye@gmail.com>
2023-08-05 11:10:36 +0300

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./login-page/public/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     screens: {
       sm: '480px',
