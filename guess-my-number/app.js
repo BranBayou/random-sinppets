af2ad25db614e0cfcf7f4df6a4769de5df3886e5
BranBayou <berhan.baye@gmail.com>
2023-08-08 20:11:53 +0300

    Add logics to guess my number

--- guess-my-number/app.js
+++ guess-my-number/app.js
@@ -1,1 +1,50 @@
 'use strict';
+
+const again = document.querySelector('.again');
+const check = document.querySelector('.check');
+const guess = document.querySelector('.guess');
+const number = document.querySelector('.number');
+const message = document.querySelector('.message');
+const scoreLabel = document.querySelector('.label-score');
+const highScoreLabel = document.querySelector('.label-highscore');
+
+const randNumber = Math.trunc(Math.random() * 20) + 1;
+number.textContent = randNumber;
+
+let score = 20;
+let highScore = 0;
+
+check.addEventListener('click', () => {
+  const guessedValue = guess.value;
+  
+
+  if(!guessedValue) {
+    message.textContent = 'Enter only numbers';
+  } else if(guessedValue == randNumber) {
+    message.textContent = 'Correct number ):';
+    number.style.width = '7rem';
+    if(score > highScore) {
+      highScore = score;
+      highScoreLabel.textContent = `High Score: ${highScore}`;
+    }
+  } else if(guessedValue > randNumber) {
+    if(score > 1) {
+      message.textContent = 'Too high!';
+      score -= 1;
+      scoreLabel.textContent = score;
+    } else {
+      message.textContent = 'You lost!';
+      scoreLabel.textContent = 0;
+    }
+  } else if(guessedValue < randNumber) {
+    if(score > 1) {
+      message.textContent = 'Too low!';
+      score -= 1;
+      scoreLabel.textContent = score;
+    } else {
+      message.textContent = 'You lost!';
+      scoreLabel.textContent = 0;
+    }
+    
+  } 
+});
