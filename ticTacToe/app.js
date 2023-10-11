e57355fc9beb73f4c3bd44ea4730f0f8dab9edf2
BranBayou <berhan.baye@gmail.com>
2023-10-11 17:16:33 +0300

    Add winning combos

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -4,6 +4,9 @@ const startCell = [
   "", "", "", "", "", "", "", "", ""
 ];
 
+let go = "circle";
+infoDisplay.textContent = "Circle goes first";
+
 function createBoard() {
   startCell.forEach((cell, index) => {
     const cellElement = document.createElement("div");
@@ -18,6 +21,37 @@ createBoard();
 
 function addGo(e) {
   const goDisplay = document.createElement("div");
-  goDisplay.classList.add("cross");
+  goDisplay.classList.add(go);
   e.target.append(goDisplay);
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = "it is now" + go +"'s go";
+  e.target.removeEventListener("click", addGo);
+  checkScore();
+}
+
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square");
+  const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  ];
+
+  winningCombos.forEach(array => {
+    const circleWins = array.every(cell => 
+      allSquares[cell].firstChild?.classList.contains("circle"));
+    if (circleWins) {
+      infoDisplay.textContent = "Circle Wins";
+      allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+      return;
+    }
+  })    
+
+  winningCombos.forEach(array => {
+    const crossWins = array.every(cell => 
+      allSquares[cell].firstChild?.classList.contains("cross"));
+    if (crossWins) {
+      infoDisplay.textContent = "Cross Wins";
+      allSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+      return;
+    }
+  })  
 }
