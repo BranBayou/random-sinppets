21c6d6d38cad1f0609589475733fb4f3848a3648
BranBayou <berhan.baye@gmail.com>
2023-10-19 07:48:13 +0300

    Add more methods

--- login-popup/app.js
+++ login-popup/app.js
@@ -1,6 +1,8 @@
+const body = document.querySelector("body");
 const modal = document.querySelector(".login-modal");
 const loginBtn = document.querySelector(".login");
 
 loginBtn.addEventListener('click', () => {
   modal.showModal();
 });
+
