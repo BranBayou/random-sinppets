043450a5dd114cec7740f42d07a1de39b40d851b
Berhanu Baye <berhan.baye@gmail.com>
2024-07-27 18:49:04 +0300

    Add ref vue attribute

--- vue-essentials/main.js
+++ vue-essentials/main.js
@@ -1,16 +1,17 @@
 Vue.createApp({
     data() {
         return {
-            newGoal: '',
             goals: [],
         }
     },
     methods: {
         addNewGoal() {
-            if (this.newGoal.trim() !== '') {
-                this.goals.push(this.newGoal);
-                this.newGoal = '';
+            const goalText = this.$refs.goalText.value.trim();
+            if (goalText !== '') {
+                this.goals.push(goalText);
+                this.$refs.goalText.value = '';
             }
         }
     }
 }).mount('#main-div');
+
