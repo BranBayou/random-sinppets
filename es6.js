511e13a77f78da1a50a5029bad5abb7c3c96e049
Berhanu Baye <berhan.baye@gmail.com>
2024-12-04 09:07:45 +0300

    Add js object methods

---
+++ es6.js
@@ -0,0 +1,10 @@
+const person = {
+    id: 11,
+    pro: 'dev',
+    salary: 3000
+}
+
+console.log('Object Keys', Object.keys(person));
+console.log('Object Values', Object.values(person));
+
+console.log('Object Entries', Object.entries(person));
